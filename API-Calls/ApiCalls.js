13ae1962b88a7c1f82aa17b7ebf66d3d4ccf7c9c
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-05-31 23:17:00 -0500

    updated documentation

--- API-Calls/ApiCalls.js
+++ API-Calls/ApiCalls.js
@@ -1,4 +1,4 @@
-fetchButton = document.getElementById("fetch-button");
+fetchButton = document.getElementById("fetch-button"); //needed global variables
 input = document.getElementById("user-input");
 errorMessage = document.getElementById("error-msg");
 
@@ -8,13 +8,13 @@ fetchButton.addEventListener("click", () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`) //fetch a pokemon based on provided name
     .then(response => { //log a response then turn it into json
         console.log(response); 
-        if(errorMessage.textContent === "Error: Pick a real pokemon"){
+        if(errorMessage.textContent === "Error: Pick a real pokemon"){ //remove error message
             errorMessage.textContent = "";
         }
-        return response.json();
+        return response.json(); //turn fetched data into JSON, this is also promise based
     }) 
     .then(data => {
-        console.log(data);
+        console.log(data); //log the data to the console then add pictures of the pokemon in the appropriate divs
         const NormalSprite = data.sprites.front_default;
         const ShinySprite = data.sprites.front_shiny;
         const NormalDisplay = document.getElementById("normal");
@@ -28,7 +28,7 @@ fetchButton.addEventListener("click", () => {
         const ShinyDisplay = document.getElementById("shiny");
         NormalDisplay.src = "red-x.png";
         ShinyDisplay.src = "red-x.png";
-        errorMessage.textContent = "Error: Pick a real pokemon"
+        errorMessage.textContent = "Error: Pick a real pokemon" //display error to user
     })
 })
 //when enter key is pressed while user is in input field submit button will be clicked
