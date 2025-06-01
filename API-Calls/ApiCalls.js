d2b7b49b1cb2c652cf6dd93afa40fc99bdd62524
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-05-31 22:49:14 -0500

    added input functionality

--- API-Calls/ApiCalls.js
+++ API-Calls/ApiCalls.js
@@ -1,18 +1,24 @@
-fetch("https://pokeapi.co/api/v2/pokemon/sudowoodo")
-.then(response => { //log a response then turn it into json
-    console.log(response); 
-    return response.json();
-}) 
-.then(data => {
-    console.log(data);
-    const NormalSprite = data.sprites.front_default;
-    const ShinySprite = data.sprites.front_shiny;
-    const NormalDisplay = document.getElementById("normal");
-    const ShinyDisplay = document.getElementById("shiny");
+fetchButton = document.getElementById("fetch-button");
 
-    NormalDisplay.src = NormalSprite;
-    ShinyDisplay.src = ShinySprite
+fetchButton.addEventListener("click", () => {
+    input = document.getElementById("user-input");
+    inputValue = input.value.toLowerCase();
 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+    .then(response => { //log a response then turn it into json
+        console.log(response); 
+        return response.json();
+    }) 
+    .then(data => {
+        console.log(data);
+        const NormalSprite = data.sprites.front_default;
+        const ShinySprite = data.sprites.front_shiny;
+        const NormalDisplay = document.getElementById("normal");
+        const ShinyDisplay = document.getElementById("shiny");
+        NormalDisplay.src = NormalSprite;
+        ShinyDisplay.src = ShinySprite;
+    })
+    .catch(error => console.error(error))
 })
-.catch(error => console.error(error))
+
 
